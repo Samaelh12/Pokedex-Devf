acf83409ce16dda5bebcadab6817be250c7ede0f
Samael <samaelsth12@gmail.com>
2023-02-27 17:51:42 -0600

    pokedex end

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,10 @@
-const pokeCard = document.querySelector('[data-poke-card]')
-const pokeName = document.querySelector('[data-poke-name]')
-const pokeImg = document.querySelector('[data-poke-img]')
-const pokeImgContainer = document.querySelector('[data-poke-img-container]')
-const pokeId = document.querySelector('[data-poke-id]')
-const pokeTypes = document.querySelector('[data-poke-types]')
-const pokeStats = document.querySelector('[poke-data-stars]')
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
 
 const typeColors = {
     electric: '#FFEA70',
@@ -26,19 +26,65 @@ const typeColors = {
     default: '#2A1A1F',
 };
 
+
 const searchPokemon = event => {
     event.preventDefault();
     const { value } = event.target.pokemon;
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
         .then(data => data.json())
         .then(response => renderPokemonData(response))
+        .catch(err => renderNotFound())
 }
 
 const renderPokemonData = data => {
-    const sprite = data.sprites.front_dafault;
-    const { stats, types} = data;
-    
+    const sprite =  data.sprites.front_default;
+    const { stats, types } = data;
+
     pokeName.textContent = data.name;
-    pokeImg.setAttribute('src', sprite)
+    pokeImg.setAttribute('src', sprite);
     pokeId.textContent = `Nº ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
+
+
+const setCardColor = types => {
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+    pokeImg.style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+    pokeImg.style.backgroundSize = ' 5px 5px';
+}
+
+const renderPokemonTypes = types => {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+    });
+}
+
+const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+    });
+}
+
+const renderNotFound = () => {
+    pokeName.textContent = 'No encontrado';
+    pokeImg.setAttribute('src', 'poke-shadow.png');
+    pokeImg.style.background =  '#fff';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+    pokeId.textContent = '';
 }
